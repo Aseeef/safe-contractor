950d56c915aec4a6051afec8cf8c4e16e7e840d2
isun3071 <isun@bu.edu>
2025-02-23 07:56:20 -0500

    fixed bugs relating to resolveDispatcher() being null

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 // App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './HomePage';
+import ReliableContractorProfile from './ReliableContractorProfile';
 
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/contractor/" element={<ReliableContractorProfile />} />
+      </Routes>
+    </Router>
   );
 }
 
